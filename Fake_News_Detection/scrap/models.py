7d2e0546b415edaf05da20e84b2df33310be61ae
NightFury <nightfuryno9@gmail.com>
2024-05-23 13:16:22 +0600

    Complete New with django database created

---
+++ Fake_News_Detection/scrap/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class news(models.Model):
+    website = models.CharField(max_length=255)
+    title = models.CharField(max_length=500)
+    snippet = models.CharField(max_length=1000)
+    link = models.CharField(max_length=1000)
+

e79f18def57afc9d624873483dc69f91ec79af2b
NightFury <nightfuryno9@gmail.com>
2024-05-26 14:31:39 +0600

    New shortcut for toogling in js

--- Fake_News_Detection/productionfiles/js/checker.js
+++ Fake_News_Detection/productionfiles/js/checker.js
@@ -1,14 +1,19 @@
+// function show(id){
+//     let x = document.getElementById(id).style
+
+//     if(x.width == '4vw'){
+//         x.width = '30vw';
+//         x.opacity = "0.8";
+//     }
+//     else{
+//         x.width = '4vw';
+//         x.opacity = '0';
+//     }
+
+//     // x.style.width = x.style.width == '30vw' ? x.style.width = '4vw' : x.style.width = '30vw';
+// }
+
 function show(id){
-    let x = document.getElementById(id).style
-
-    if(x.width == '4vw'){
-        x.width = '30vw';
-        x.opacity = "0.8";
-    }
-    else{
-        x.width = '4vw';
-        x.opacity = '0';
-    }
-
-    // x.style.width = x.style.width == '30vw' ? x.style.width = '4vw' : x.style.width = '30vw';
+    let x = document.getElementById(id);
+    Element.classList.toggle('list-new');
 }
